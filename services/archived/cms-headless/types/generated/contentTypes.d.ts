584784c81153e9acbeaf657dcc96cf8e1e0a5b58
Doitsu Technology - Duc TH <thd1152015@gmail.com>
2023-10-09 11:59:10 +0700

    Change

--- services/archived/cms-headless/types/generated/contentTypes.d.ts
+++ services/archived/cms-headless/types/generated/contentTypes.d.ts
@@ -23,6 +23,7 @@ export interface AdminPermission extends Schema.CollectionType {
       Attribute.SetMinMaxLength<{
         minLength: 1;
       }>;
+    actionParameters: Attribute.JSON & Attribute.DefaultTo<{}>;
     subject: Attribute.String &
       Attribute.SetMinMaxLength<{
         minLength: 1;
@@ -676,7 +677,7 @@ export interface PluginUsersPermissionsUser extends Schema.CollectionType {
   };
 }
 
-declare module '@strapi/strapi' {
+declare module '@strapi/types' {
   export module Shared {
     export interface ContentTypes {
       'admin::permission': AdminPermission;
