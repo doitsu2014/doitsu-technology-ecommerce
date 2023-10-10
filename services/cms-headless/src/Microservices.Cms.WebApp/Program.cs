9bd56d8f827b318d31a5eb8bccdad1de2fd2ef79
Doitsu Technology - Duc TH <thd1152015@gmail.com>
2023-10-10 17:33:11 +0700

    Update

--- services/cms-headless/src/Microservices.Cms.WebApp/Program.cs
+++ services/cms-headless/src/Microservices.Cms.WebApp/Program.cs
@@ -14,11 +14,65 @@ builder.Host.UseSerilog((hostingContext, configBuilder) =>
 
 builder.Services
     .AddOrchardCms()
+    .AddGlobalFeatures(
+        "OrchardCore.Apis.GraphQL",
+        "OrchardCore.Commerce.ContentFields",
+        "OrchardCore.Commerce",
+        "OrchardCore.Commerce.CurrencySettingsSelector",
+        "OrchardCore.Commerce.Tax.CustomTaxRates",
+        "OrchardCore.Commerce.Inventory",
+        "OrchardCore.Commerce.Promotion",
+        "OrchardCore.Commerce.SessionCartStorage",
+        "OrchardCore.Commerce.Tax",
+        "OrchardCore.Feeds",
+        "OrchardCore.Flows",
+        "OrchardCore.Forms",
+        "OrchardCore.Layers",
+        "OrchardCore.Shortcodes.Templates",
+        "OrchardCore.Widgets",
+        "OrchardCore.Contents.Deployment.AddToDeploymentPlan",
+        "OrchardCore.AdminDashboard",
+        "OrchardCore.AdminMenu",
+        "OrchardCore.Alias",
+        "OrchardCore.ArchiveLater",
+        "OrchardCore.ContentFields",
+        "OrchardCore.ContentFields.Indexing.SQL",
+        "OrchardCore.ContentFields.Indexing.SQL.UserPicker",
+        "OrchardCore.ContentPreview",
+        "OrchardCore.ContentTypes",
+        "OrchardCore.Contents",
+        "OrchardCore.Contents.Deployment.ExportContentToDeploymentTarget",
+        "OrchardCore.Contents.FileContentDefinition",
+        "OrchardCore.Html",
+        "OrchardCore.Liquid",
+        "OrchardCore.Lists",
+        "OrchardCore.Markdown",
+        "OrchardCore.Media",
+        "OrchardCore.Media.Cache",
+        "OrchardCore.Media.Slugify",
+        "OrchardCore.PublishLater",
+        "OrchardCore.Queries",
+        "OrchardCore.Seo",
+        "OrchardCore.Sitemaps",
+        "OrchardCore.Sitemaps.Cleanup",
+        "OrchardCore.Sitemaps.RazorPages",
+        "OrchardCore.Spatial",
+        "OrchardCore.Queries.Sql",
+        "OrchardCore.Taxonomies",
+        "OrchardCore.Taxonomies.ContentsAdminList",
+        "OrchardCore.Title",
+        "OrchardCore.Contents.Deployment.Download",
+        "OrchardCore.Tenants.Distributed",
+        "OrchardCore.Redis",
+        "OrchardCore.Redis.Bus",
+        "OrchardCore.Redis.Cache",
+        "OrchardCore.Redis.DataProtection",
+        "OrchardCore.Redis.Lock",
+        "OrchardCore.AutoSetup")
     .AddDatabaseShellsConfiguration()
+
     // Orchard Specific Pipeline
-    .ConfigureServices(services =>
-    {
-    })
+    .ConfigureServices(services => { })
     // .Configure((app, routes, services) => { });
     ;
 var app = builder.Build();
