5f82e30c2faf62cbdb8b0c225d484e2682e7bfe9
Doitsu Technology - Duc TH <thd1152015@gmail.com>
2023-10-11 10:33:43 +0700

    ok

---
+++ services/cms-headless/src/OrchardCore.Dtech.Recipes/Manifest.cs
@@ -0,0 +1,15 @@
+﻿using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Id = "OrchardCore.Dtech.Cms",
+    Name = "Dtech Cms Module",
+    Author = "Doitsu Technology",
+    Website = "https://doitsu.tech",
+    Version = "0.0.1",
+    Description = "Provides recipes.",
+    Category = "Recipes",
+    Dependencies = new[]
+    {
+        "OrchardCore.Recipes"
+    })
+]
---
+++ services/cms-headless/src/OrchardCore.Dtech.Recipes/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OrchardCore.Dtech.Recipes;
+
+public class Startup : StartupBase
+{
+    public override void Configure(IApplicationBuilder app)
+    {
+    }
+
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        base.ConfigureServices(services);
+    }
+}
