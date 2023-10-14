93a3986cf828c9f6a78419cb13778899e24eadb3
thd1152015@gmail.com <dtran@doitsu.tech>
2023-10-14 11:38:25 +0700

    Update

--- services/cms-headless/src/DTech.Cms.Abstractions/ManifestConstants.cs
+++ services/cms-headless/src/DTech.Cms.Abstractions/ManifestConstants.cs
@@ -4,6 +4,6 @@ public class ManifestConstants
 {
     public const string ManifestAuthor = "D Tech";
     public const string ManifestWebsite = "https://doitsu.tech";
-    public const string ManifestVersion = "0.0.1";
+    public const string ManifestVersion = "1.7.0";
     public const string ManifestCategory = "Doitsu Technology";
 }
---
+++ services/cms-headless/src/DTech.Cms.Commerce.Module/Manifest.cs
@@ -0,0 +1,11 @@
+using OrchardCore.Modules.Manifest;
+using ManifestConstants = DTech.Cms.Abstractions.ManifestConstants;
+
+[assembly: Module(
+    Name = "DTech.Cms.Commerce.Module",
+    Author = ManifestConstants.ManifestAuthor,
+    Website = ManifestConstants.ManifestWebsite,
+    Version = ManifestConstants.ManifestVersion,
+    Description = "DTech.Cms.Commerce.Module",
+    Category = ManifestConstants.ManifestCategory
+)]
