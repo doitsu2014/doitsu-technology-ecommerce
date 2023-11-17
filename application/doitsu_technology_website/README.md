5bc142e8b823deec55fbddbc7fba8d7428c1280b
thd1152015@gmail.com <dtran@doitsu.tech>
2023-11-17 23:02:39 +0700

    Update VSCode Workspace

---
+++ application/doitsu_technology_website/README.md
@@ -0,0 +1,6 @@
+# Introduce
+
+# 1. Install and Setup
+
+- The first tool need to setup in there project is `bonnie`, you can install it using `cargo install bonnie`
+- The second tool need to setup in there project is `npm`.
--- application/doitsu_technology_website/dtech_csr/tailwind.config.js
+++ application/doitsu_technology_website/dtech_csr/tailwind.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-    content: [
-        "./src/**/*.rs",
-        "./index.html",
-        "*.html",
-        "*.css",
-    ],
+    content: ["./src/**/*.rs", "./index.html", "*.html", "*.css"],
     theme: {},
     variants: {},
     plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/aspect-ratio"),
+        require("@tailwindcss/container-queries"),
+    ]
 };
