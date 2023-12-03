48064406f05f17b85184af2cf02a742f761599a2
ドイツ <thd1152015@gmail.com>
2023-12-03 20:39:49 +0700

    Update SSR

--- application/doitsu_technology_website/src/bin/dtech_csr.rs
+++ application/doitsu_technology_website/src/bin/dtech_csr.rs
@@ -1,4 +1,4 @@
-pub use dtech_csr::*;
+use dtech_website::App;
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
--- application/doitsu_technology_website/src/bin/dtech_hydrate.rs
+++ application/doitsu_technology_website/src/bin/dtech_hydrate.rs
@@ -1,6 +1,7 @@
-pub use dtech_csr::*;
+use dtech_website::App;
 
 fn main() {
+    #[cfg(target_arch = "wasm32")]
     wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
-    yew::Renderer::<App>::new().render();
+    yew::Renderer::<App>::new().hydrate();
 }
